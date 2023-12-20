a3fa7ad34de4bb6de9ebebb10f365c43df8a8dfd
sem pruijs <sem@pruijs.net>
2023-12-20 17:12:57 +0100

    make shoelace function

--- shoelace/src/lib.rs
+++ shoelace/src/lib.rs
@@ -1,5 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl From<(i64, i64)> for Point {
+    fn from(pair: (i64, i64)) -> Self {
+        Point {
+            x: pair.0,
+            y: pair.1,
+        }
+    }
+}
+
+pub struct Area(f64);
+
+impl From<Vec<Point>> for Area {
+    fn from(points: Vec<Point>) -> Self {
+        let mut result = 0;
+        let points = points.into_iter().rev().collect::<Vec<Point>>();
+        let start_point = &points[0];
+        for (i, point) in points.iter().enumerate() {
+            let next_point = match points.len() - 1 == i {
+                true => &start_point,
+                false => &points[i + 1],
+            };
+            let area = (point.x * next_point.y) - (point.y * next_point.x);
+            dbg!(&area);
+            result += area;
+        }
+        let total_area = result as f64 * 0.5;
+        Area(total_area)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +38,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_points_to_area() {
+        let points = vec![
+            Point::from((4, 4)),
+            Point::from((5, -2)),
+            Point::from((-1, -4)),
+            Point::from((-6, 0)),
+            Point::from((-2, 5)),
+            Point::from((0, 1)),
+        ];
+
+        let r_1 = Area::from(points).0;
+        let expected_1: f64 = 55.0;
+        assert_eq!(r_1, expected_1);
     }
 }
